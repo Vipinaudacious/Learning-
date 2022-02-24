f9e6196a61e19a307d8a8d76b4e9707a83598f4a
Vipinaudacious <98941022+Vipinaudacious@users.noreply.github.com>
2022-02-24 19:05:43 +0530

    Add files via upload
    java scripts string and its methods

---
+++ index.js
@@ -0,0 +1,32 @@
+//  var str = "this  for simple set of character 1243 is";
+// var str2  = " heloow worlds";
+// var str3  = " heloow this is  worlds";
+var s = "disfor";
+
+ //var b = str.toUpperCase();
+//document.write(b);
+
+//var b = str.toLowerCase();
+//var b = str.includes("123");
+// var b = str.startsWith("  this");
+// var b = str.search("of");
+//var b = str.match(/is/g);
+// var b = str.indexOf("is");
+// var b = str.replace("thisa","that");
+//  var b = str.replace("thisa","that");
+//  var b = str.trim()
+// alert(b)
+// var b = str.charAt("2");
+// var b = str.charCodeAt("2");
+// var b = String.fromCharCode(65);
+//  var b = str.concat(str2,str3);
+//  var b = str.split(" ");
+//  var b = str.repeat(5);
+//  var b = str.subs(3);
+//  var b = str.(1,7);
+// var  str = 50;
+//   var b = str.t;
+// var  b = str.length;
+ var b = s.padStart(10, "*");
+document.write(b);  
+//  console.log(b);
