e194490b44a55bf5527ebf71adf6789b374af42b
Vipinaudacious <98941022+Vipinaudacious@users.noreply.github.com>
2022-03-12 00:11:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-//  var str = "this  for simple set of character 1243 is";
-// var str2  = " heloow worlds";
-// var str3  = " heloow this is  worlds";
-var s = "disfor";
+// This is conform box
+/*const  a = confirm(" Are you sure you want to submit the form")
 
- //var b = str.toUpperCase();
-//document.write(b);
 
-//var b = str.toLowerCase();
-//var b = str.includes("123");
-// var b = str.startsWith("  this");
-// var b = str.search("of");
-//var b = str.match(/is/g);
-// var b = str.indexOf("is");
-// var b = str.replace("thisa","that");
-//  var b = str.replace("thisa","that");
-//  var b = str.trim()
-// alert(b)
-// var b = str.charAt("2");
-// var b = str.charCodeAt("2");
-// var b = String.fromCharCode(65);
-//  var b = str.concat(str2,str3);
-//  var b = str.split(" ");
-//  var b = str.repeat(5);
-//  var b = str.subs(3);
-//  var b = str.(1,7);
-// var  str = 50;
-//   var b = str.t;
-// var  b = str.length;
- var b = s.padStart(10, "*");
-document.write(b);  
-//  console.log(b);
+if(a){
+     alert(" thanks for submitting from");
+}
+else{
+     alert(" Plz fill the  form correctly !");
+}
+*/
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'abc'];
+// let arr2 = ['b', 'a', 'c', 'd', 'e'];
+// document.write(arr2,arr)
+// document.write('arr length --', arr.length) // 11
+//  document.write(arr.pop()) // it will pop last element from the array
+// document.write(arr.push()) // it will push element at the starting position
+// document.write(arr.shift()) // it will remove first element from the array and return shifted element
+// document.write(arr.unshift('cde')) // method adds a new element to an array (at the beginning), and "unshifts" older elements
+// document.write(arr.splice(10, 1, 11)) // The splice() method can be used to add new items to an array and it is return deleted items
+// document.write(arr2.sort()) // its sorts element in ascending order but for number it will give us a wrong sorting order
+// document.write(arr2.sort()) // now it will work properly
+// document.write(arr.concat(arr2)) // it will join element
+// document.write(arr.slice(0, 5)) // it is return sliced items and it takes two parameter (start, end)
+// document.write(arr[0]) // access element by indexing and indexing will start from zero(0)
+// let result = arr.toString() // output - 1,2,3,4,5,6,7,8,9,10,abc
+// document.write(typeof result) // it gives us type of as string
+//document.write(arr.indexOf(5)) // it will give us index and if not exist, then it will return -1
+// document.write(arr.lastIndexOf(10)) // it will give us last given item index and it will give preference to last visit
+document.write(arr.includes(2)) // it will give us boolean output if given item is exist, otherwise it will give us false
+// document.write(arr.splice(10, 1, 11))
+// document.write(arr);
